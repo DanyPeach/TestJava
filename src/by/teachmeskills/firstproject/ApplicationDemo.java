98a43393ee9c14df990141215ca14cfe36042a15
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-10 22:06:00 +0300

    add simple examples

--- src/by/teachmeskills/firstproject/ApplicationDemo.java
+++ src/by/teachmeskills/firstproject/ApplicationDemo.java
@@ -3,5 +3,17 @@ package by.teachmeskills.firstproject;
 public class ApplicationDemo {
     public static void main(String[] args) {
         System.out.println("I'm Vegan and your not lol! Haha");
+        long a = 2;
+        double d = a-4;
+        System.out.println(d + " & " + a);
+
+        double h = 2.5;
+        int r = (int)h;
+        System.out.println(r);
+
+        int y = 9;
+        int c = ++y;
+        int e = y++;
+        System.out.println(c + " " + e);
     }
 }
