21cd4927d426b8418708f6dd25c0b546b6407a08
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-15 19:38:16 +0300

    new lesson add

--- src/by/teachmeskills/lessom3pr/OprTasks.java
+++ src/by/teachmeskills/lessom3pr/OprTasks.java
@@ -1,4 +1,10 @@
 package by.teachmeskills.lessom3pr;
 
 public class OprTasks {
+    public static void main(String[] args) {
+        int a = 5;
+        if(a>0){
+            System.out.println("Yes");
+        }
+    }
 }
